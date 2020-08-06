82625f28aeb1cc95a539b1f1ab239d64385161fc
mz03-jmryu <mz03-jmryu@10.162.173.15>
2020-08-06 22:29:16 +0900

    aa

--- src/test/java/com/chicor/DockerChromeJupiterTest.java
+++ src/test/java/com/chicor/DockerChromeJupiterTest.java
@@ -71,13 +71,10 @@ public class DockerChromeJupiterTest {
     	WebDriver driver;
         
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("headless");
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("headless");
+        options.addArguments("--single-process"); 
+        options.addArguments("--disable-dev-shm-usage");
+        
 		driver = new ChromeDriver(options);
 
 		try {
