86ed9bf495d1b50b0a04e499a7e59ca3b4f287dd
mz03-jmryu <mz03-jmryu@10.162.173.15>
2020-08-06 21:44:15 +0900

    aa

--- src/test/java/com/chicor/DockerChromeJupiterTest.java
+++ src/test/java/com/chicor/DockerChromeJupiterTest.java
@@ -16,6 +16,8 @@
  */
 package com.chicor;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -23,7 +25,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DockerChromeJupiterTest {
 
@@ -39,29 +43,52 @@ public class DockerChromeJupiterTest {
     	System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver");
     	
     	
-
+    	//Selenium selenium = new DefaultSelenium("localhost", 4444, "*googlechrome", "http://www.google.com");
+    	//DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    	//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
         
+    	
+    	//Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://my.test.site.org/");
+    	//Selenium 2 clients should connect as follow:
+
+    	//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+    	
     }	
 	
 	
     @Test
     public void GNBTest() {
     	
-    	WebDriver driver = new ChromeDriver();
+    	//WebDriver driver = new ChromeDriver();
     	
-    	 
-        driver.get("https://chicor.com/main");
-        driver.manage().window().setSize(new Dimension(1516, 737));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.linkText("오늘 하루 보지 않기")).click();
-        driver.findElement(By.linkText("닫기")).click();
-        driver.findElement(By.linkText("오늘 하루 보지 않기")).click();
-        driver.findElement(By.linkText("닫기")).click();
-        driver.findElement(By.linkText("BEST")).click();
-        driver.findElement(By.linkText("BRANDS")).click();
-        driver.findElement(By.linkText("STORY")).click();
-        driver.findElement(By.linkText("DEAL")).click();
-        driver.close();
+    	ChromeOptions options = new ChromeOptions();
+    	options.addArguments("--start-maximized");
+    	DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    	capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+    	WebDriver driver;
+		try {
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+			
+			
+	        driver.get("https://chicor.com/main");
+	        driver.manage().window().setSize(new Dimension(1516, 737));
+	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	        driver.findElement(By.linkText("오늘 하루 보지 않기")).click();
+	        driver.findElement(By.linkText("닫기")).click();
+	        driver.findElement(By.linkText("오늘 하루 보지 않기")).click();
+	        driver.findElement(By.linkText("닫기")).click();
+	        driver.findElement(By.linkText("BEST")).click();
+	        driver.findElement(By.linkText("BRANDS")).click();
+	        driver.findElement(By.linkText("STORY")).click();
+	        driver.findElement(By.linkText("DEAL")).click();
+	        driver.close();
+	        
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    			
+
     }
 
 	 
