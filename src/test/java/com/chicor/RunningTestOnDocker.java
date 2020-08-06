8800f9a953689e33341e57a96255ce12e9b89f32
mz03-jmryu <mz03-jmryu@10.162.173.15>
2020-08-06 22:46:21 +0900

    aa

---
+++ src/test/java/com/chicor/RunningTestOnDocker.java
@@ -0,0 +1,27 @@
+package com.chicor;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class RunningTestOnDocker {
+
+    @Test
+    public void runTestOnDocker() throws Exception {
+        DesiredCapabilities dcap = DesiredCapabilities.chrome();
+       // String driverPath = System.getProperty("user.dir") + "/chromedriver/chromedriver";
+        String driverPath = "/chromedriver/chromedriver";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+
+        // You should check the Port No here.
+        URL gamelan = new URL("http://localhost:4444/wd/hub");
+        WebDriver driver = new RemoteWebDriver(gamelan, dcap);
+        // Get URL
+        driver.get("https://www.google.com/");
+        // Print Title
+        System.out.println(driver.getTitle());
+    }
+}
